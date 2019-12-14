752a8a428fa9d68c4eebf58566c56b601016cd9f
Sava <ionut_sava10@yahoo.com>
2019-12-14 18:29:23 +0200

    Blocks and declarations

--- opl.y
+++ opl.y
@@ -5,16 +5,75 @@ extern int yylineno;
 %}
 
 %token INT FLOAT CHAR STRING BOOL
+	   BEGIN_DECL END_DECL BEGIN_MAIN END_MAIN
 	   ID 
-	   UNSIGNED_NUMBER FLOAT_NUMBER
-	   STRING_VALUE BOOL_VALUE
+	   UNSIGNED_NUMBER INTEGER_NUMBER FLOAT_NUMBER
+	   STRING_VALUE BOOL_VALUE CHAR_VALUE
 %start s
 
 %%
 
 s: progr {printf("Input corect sintactic\n");}
+ ;
 
-progr:
+progr: declarations main
+     | main
+     ;
+
+declarations: BEGIN_DECL declarations_content END_DECL
+            ;
+
+declarations_content: decl 
+                    | decl declarations_content
+                    ;
+
+decl: var_decl ';'
+    ;
+
+var_decl: type ID
+        | type ID '=' value
+		| type ID '[' UNSIGNED_NUMBER ']'
+		| type ID '[' UNSIGNED_NUMBER ']' '=' '{' '}'
+		| type ID '[' UNSIGNED_NUMBER ']' '=' '{' array '}'
+        ;
+
+type: INT
+    | FLOAT
+    | CHAR
+    | STRING
+    | BOOL
+    ;
+
+value: INTEGER_NUMBER
+     | UNSIGNED_NUMBER
+     | FLOAT_NUMBER
+     | STRING_VALUE
+     | BOOL_VALUE
+     | CHAR_VALUE
+     ;
+
+array: array_element
+     | array_element ',' array
+     ;
+
+array_element: ID
+             | value
+             ;
+
+main: BEGIN_MAIN main_content END_MAIN
+    ;
+
+main_content: instruction ';'
+            | instruction ';' main_content
+            ;
+
+instruction: assign
+           ;
+
+assign: ID '=' ID 
+      | ID '=' value
+      | ID '[' UNSIGNED_NUMBER ']' '=' value
+      ;
 
 %%
 
---
+++ y.tab.h
@@ -0,0 +1,96 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    INT = 258,
+    FLOAT = 259,
+    CHAR = 260,
+    STRING = 261,
+    BOOL = 262,
+    BEGIN_DECL = 263,
+    END_DECL = 264,
+    BEGIN_MAIN = 265,
+    END_MAIN = 266,
+    ID = 267,
+    UNSIGNED_NUMBER = 268,
+    INTEGER_NUMBER = 269,
+    FLOAT_NUMBER = 270,
+    STRING_VALUE = 271,
+    BOOL_VALUE = 272,
+    CHAR_VALUE = 273
+  };
+#endif
+/* Tokens.  */
+#define INT 258
+#define FLOAT 259
+#define CHAR 260
+#define STRING 261
+#define BOOL 262
+#define BEGIN_DECL 263
+#define END_DECL 264
+#define BEGIN_MAIN 265
+#define END_MAIN 266
+#define ID 267
+#define UNSIGNED_NUMBER 268
+#define INTEGER_NUMBER 269
+#define FLOAT_NUMBER 270
+#define STRING_VALUE 271
+#define BOOL_VALUE 272
+#define CHAR_VALUE 273
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
